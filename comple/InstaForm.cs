753db94cc388d1cbe8cec11b1cfe0c0cfd6cbe97
Raúl Medina <raul.medina@catolica.edu.sv>
2024-04-25 21:21:49 -0600

    Diseño Insta

---
+++ comple/InstaForm.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp1
+{
+    public partial class InstaForm : Form
+    {
+        public InstaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ucNewsFeed2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
